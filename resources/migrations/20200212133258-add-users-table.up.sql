023beb001972015914022aa916f6a53cfe61b2e5
Dave Simmons <shortlyportly@googlemail.com>
2020-02-13 21:17:03 +0000

    added edn files

--- resources/migrations/20200212133258-add-users-table.up.sql
+++ resources/migrations/20200212133258-add-users-table.up.sql
@@ -2,8 +2,4 @@ CREATE TABLE users
 (id VARCHAR(20) PRIMARY KEY,
  first_name VARCHAR(30),
  last_name VARCHAR(30),
- email VARCHAR(30),
- admin BOOLEAN,
- last_login TIMESTAMP,
- is_active BOOLEAN,
- pass VARCHAR(300));
+ email VARCHAR(30));
--- src/clj/playpen/routes/user.clj
+++ src/clj/playpen/routes/user.clj
@@ -1,5 +1,14 @@
 (ns playpen.routes.user)
 
+(def user-schema
+  [[:first_name st/required st/string]
+  [:last_name st/required st/string]
+  [:email st/required st/email]])
+
+(defn validate-message [params]
+  "Validate the given parameters against the user schema"
+  (first (st/validate params user-schema)))
+
 (defn create-user! [{:keys [params]}]
   "Create a user record."
   (if-let [errors (validate-user params)]
